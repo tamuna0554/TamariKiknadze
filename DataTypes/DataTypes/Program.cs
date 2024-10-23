92b53370d679368fce3316425c4b0be733d5734d
tamuna0554 <tamarikiknadze@credo.ge>
2024-10-23 09:36:56 +0400

    davaleba

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// short - 16,
+// int - 32,
+// long - 64, 
+// byte - 8; მაქს 2^8-1=255
+
+string FirstName = "tamuna";
+string LastName = "kiknadze";
+string fullName = FirstName + " " + LastName;
+int age = 35;
+Boolean isMarried = false;
+double height = 178.10;
+double Weight = 68.50;
+
+
+
+
+Console.WriteLine("My age is" + age);
+Console.ReadKey(true);
