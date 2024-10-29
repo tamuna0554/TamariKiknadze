b88ba6c4103959b8a9aa9677b9197330f0921774
tamuna0554 <tamarikiknadze@credo.ge>
2024-10-30 00:28:03 +0400

    5 davaleba

---
+++ Task05/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+//დავალება:
+
+//შექმენით ერთგანზომილებიანი მასივი სახელად students, რომელიც დაიტევს 3 სტუდენტის სახელს.
+//მომხმარებელმა უნდა ჩაწეროს ამ მასივში ნებისმიერი 3 სახელი.
+//დაბეჭდეთ ყველა სტუდენტის სახელი.შექმენით ერთგანზომილებიანი მასივი სახელად students, რომელიც დაიტევს 3 სტუდენტის სახელს.
+//მომხმარებელმა უნდა ჩაწეროს ამ მასივში ნებისმიერი 3 სახელი.
+//დაბეჭდეთ ყველა სტუდენტის სახელი.
+
+//string[] students = { "temo", "tamuna", "giorgi" };
+
+//Console.WriteLine(students[0]);
+
+//string a = Console.ReadLine();
+//string b = Console.ReadLine();
+//string c = Console.ReadLine();
+
+//string[] students = {a, b, c};
+
+//Console.WriteLine(students[0]);
+
+string[] Students  = new string[3];
+
+for (int i = 0; i < Students.Length; i++)
+{
+    string Name = Console.ReadLine();
+    Students[i] = Name; 
+}
+
